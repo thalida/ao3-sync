da210c12f17e0f359ce890426a7a6c09504caf75
Thalida Noel <hello@thalida.com>
2024-08-09 21:23:22 +0000

    Rename cli entry function

--- ao3_sync/cli.py
+++ ao3_sync/cli.py
@@ -4,7 +4,7 @@ import click
 @click.command()
 @click.option("--count", default=1, help="Number of greetings.")
 @click.option("--name", prompt="Your name", help="The person to greet.")
-def cli(count, name):
+def main(count, name):
     """Simple program that greets NAME for a total of COUNT times."""
     for x in range(count):
         click.echo(f"Hello {name}!")
