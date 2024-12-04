c93a00b66e0192abb30bdd9c24e96d49e09c6928
Thalida Noel <hello@thalida.com>
2024-12-04 11:08:08 -0500

    Cleanup test_script.py

--- test_script.py
+++ test_script.py
@@ -1,16 +1,3 @@
-# from ao3_sync.api import AO3ApiClient
-
-# api = AO3ApiClient(
-#     username="jessikajones",
-#     password='!^}t#hQJ"5zU10:b3c\\9MM',
-#     debug=True,
-#     use_debug_cache=False,
-#     use_history=False,
-# )
-# # api.series.fetch_works("1492355")
-# api.series.sync("1492355")
-
-
 from enum import Enum
 
 
